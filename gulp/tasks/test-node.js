6165b43452e9fd30839244807df995b5aaf8b090
Cesare Naldi <naldi.cesare@gmail.com>
2014-06-26 22:46:21 +0100

    Refactor fluid API to not require many error checking, improves browserify configuration for browser testing

--- gulp/tasks/test-node.js
+++ gulp/tasks/test-node.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
 
-require('./utils/runtime-transpiler')
-
 gulp.task('test-node', function () {
+
+	require('./utils/runtime-transpiler')
+
 	return gulp.src([ 'test/spec/**/*.spec.js' ], { read: false })
 		.pipe(mocha({reporter: 'progress'}))
 })
